7f5e4e05322ff00a0063dc1794fb0466e6be6738
ezuckerman24 <elzuckerman@packer.edu>
2023-02-13 08:43:11 -0500

    pt 3

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -22,3 +23,12 @@ def songs():
     user = {'username': 'Miguel'}
     songs = [{'title': 'Let it Be','artist': 'the Beatles'},{'title':'Billy Jean','artist': 'Michael Jackson'}]
     return render_template('songs.html', title='Songs', user=user, songs=songs)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
